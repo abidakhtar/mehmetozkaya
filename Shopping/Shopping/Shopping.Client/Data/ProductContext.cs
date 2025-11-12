b6b69e30d07d30dfca51881a11d3f89f25256761
Abid Akhtar <abid.akhtar@gmail.com>
2025-11-12 23:33:40 +0530

    Final Medlee

--- Shopping/Shopping/Shopping.Client/Data/ProductContext.cs
+++ Shopping/Shopping/Shopping.Client/Data/ProductContext.cs
@@ -46,7 +46,7 @@
             },
               new Models.Product
             {
-                Name = " Great Headphones",
+                Name = " Superb Headphones",
                 Category = "Accessories",
                 Description = "Noise-cancelling headphones for immersive sound.",
                 ImageFile = "61qFaIA630L.jpg",
